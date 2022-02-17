b2a5f3e4ff2b874089a20e3d35fac90c37d3d8f6
Kimchee <kchaase@gmail.com>
2022-02-17 00:38:13 -0700

    added droppable area

--- src/app.ts
+++ src/app.ts
@@ -190,7 +190,8 @@ class ProjectItem
   }
   @autobind
   dragStartHandler(event: DragEvent) {
-    console.log(event);
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move"; //changes cursor and informs browser
   }
   @autobind
   dragEndHandler(_: DragEvent) {
@@ -224,12 +225,17 @@ class ProjectList
   }
 
   @autobind
-  dragOverHandler(_: DragEvent) {
-    const listEl = this.element.querySelector("ul")!;
-    listEl.classList.add("droppable");
+  dragOverHandler(event: DragEvent) {
+    if (event.dataTransfer && event.dataTransfer.types[0] === "text/plain") {
+      event.preventDefault(); //default for JS D&D is to not allow drop
+      const listEl = this.element.querySelector("ul")!;
+      listEl.classList.add("droppable");
+    }
   }
 
-  dropHandler(_: DragEvent) {}
+  dropHandler(event: DragEvent) {
+    console.log(event.dataTransfer!.getData("text/plain"));
+  }
 
   @autobind
   dragLeaveHandler(_: DragEvent) {
